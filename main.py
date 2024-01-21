097ebeb61f42352a60f3e3078d68784a7a72bede
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-01-21 14:52:07 +0800

    Training Multiple linear regression model and predicting the result

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
 
 # Import the dataset
 dataset = pd.read_csv("sample_data/50_Startups.csv")
@@ -17,3 +18,12 @@ X = np.array(ct.fit_transform(X))
 
 # Splitting dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Training Multiple Linear Regression model on training set
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the result
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)), 1))
